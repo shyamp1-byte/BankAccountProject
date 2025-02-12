b9237c3282c4e00525c0d1c2f97b98e1d116cdef
Shyam <shyamie123@gmail.com>
2025-02-12 17:23:12 -0500

    Added Checkings Class

--- BankAccount.py
+++ BankAccount.py
@@ -1,9 +1,11 @@
 class BankAccount:
     bank_title = "Global Trust Bank"
-    def __init__(self, customer_name, current_balance, minimum_balance):
+    def __init__(self, customer_name, current_balance, minimum_balance, account_number, routing_number):
         self.customer_name = customer_name
         self.current_balance = current_balance
         self.minimum_balance = minimum_balance
+        self._account_number = account_number
+        self.__routing_number = routing_number
 
     def deposit(self, amount):
         self.current_balance += amount
@@ -21,3 +23,4 @@ class BankAccount:
         print(f"Customer Name: {self.customer_name}")
         print(f"Current Balance: {self.current_balance}")
         print(f"Minimum Balance: {self.minimum_balance}")
+        print(f"Account Number: {self._account_number}")
