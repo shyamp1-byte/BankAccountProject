7c85d3fb05509430ae14506275555327d095d3a8
Shyam <shyamie123@gmail.com>
2025-02-12 17:52:49 -0500

    Added main.py file

--- Checking.py
+++ Checking.py
@@ -1,7 +1,7 @@
 from BankAccount import BankAccount
 
-class Checkings(BankAccount):
-    def __int__(self, customer_name, current_balance, minimum_balance, account_number, routing_number, transaction_limit):
+class Checking(BankAccount):
+    def __init__(self, customer_name, current_balance, minimum_balance, account_number, routing_number, transaction_limit):
         super().__init__(customer_name, current_balance, minimum_balance, account_number, routing_number)
         self.transaction_limit = transaction_limit
         self.transactions_today = 0
--- Savings.py
+++ Savings.py
@@ -5,7 +5,7 @@ class Savings(BankAccount):
         super().__init__(customer_name, current_balance, minimum_balance, account_number, routing_number)
         self.interest_rate = interest_rate
 
-        def apply_interest_rate(self):
-            interest = self.current_balance * self.interest_rate/100
-            self.current_balance += interest
-            print(f"Interest applied is: ${interest}. New balance: ${self.current_balance}")
+    def apply_interest_rate(self):
+        interest = self.current_balance * self.interest_rate/100
+        self.current_balance += interest
+        print(f"Interest applied is: ${interest}. New balance: ${self.current_balance}")
